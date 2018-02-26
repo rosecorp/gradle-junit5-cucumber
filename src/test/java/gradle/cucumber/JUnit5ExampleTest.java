8b0703b5662139e0fcedf6d9b8b64191dee88c0e
jk <dakoma@gmail.com>
2018-02-26 23:08:04 +0000

    JK - sample gradle, cucumber, junit 5

--- src/test/java/gradle/cucumber/JUnit5ExampleTest.java
+++ src/test/java/gradle/cucumber/JUnit5ExampleTest.java
@@ -1,10 +1,12 @@
 package gradle.cucumber;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class JUnit5ExampleTest {
     @Test
     void justAnExample() {
         System.out.println("This test method should be run");
+        Assertions.assertEquals(2, 2);
     }
 }
